6ff8837bc45f5d39209ff536323fdd5b3dea917e
Kayla Moorcroft <kayla.moorcroft@icloud.com>
2024-05-29 17:00:44 -0300

    Sorted groceries dictionary by soonest expiry so priorities are at top of list. Moved groceries to local variable within main() for now

--- GroceryHelper/GroceryHelper.py
+++ GroceryHelper/GroceryHelper.py
@@ -2,9 +2,9 @@
 import csv
 from datetime import date
 
-groceries = {}
-
 def main():
+    groceries = {}
+    
     # read csv file and save into groceries dictionary in format "item: days left"
     with open('groceries.csv', mode = 'r') as file:
         csvFile = csv.reader(file)
@@ -12,7 +12,11 @@ def main():
             entry = lines[0].split(";")
             groceries[entry[0]] = date.fromisoformat(entry[1]) - date.today()
     
-    for item in groceries.keys():        
+    # sort by soonest expiry so priorities are at top of list
+    # sorted method returns list, so need to convert back to dict
+    groceries = dict(sorted(groceries.items(), key=lambda item: item[1]))
+    
+    for item in groceries.keys(): 
         # display item and days left - special message if expiring today or already expired
         print(item + ":", end = " ")
         daysLeft = groceries[item].days
