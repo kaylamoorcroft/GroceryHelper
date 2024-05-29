cc66e78e173868b4815b8d3650f8b8647c31cd7c
Kayla Moorcroft <kayla.moorcroft@icloud.com>
2024-05-29 16:23:39 -0300

    Changed expiry dates to days remaining. Using iso format in groceries.csv now to make conversion easier

--- GroceryHelper/GroceryHelper.py
+++ GroceryHelper/GroceryHelper.py
@@ -1,24 +1,20 @@
 
-import csv, datetime
+import csv
+from datetime import date
 
-expiryDates = {}
+daysLeft = {}
 
 def main():
-    # read csv file and save into expiryDates dictionary in format "item: expiryDate"
+    # read csv file and save into daysLeft dictionary in format "item: expiryDate"
     with open('groceries.csv', mode = 'r') as file:
         csvFile = csv.reader(file)
         for lines in csvFile:
             entry = lines[0].split(";")
-            expiryDates[entry[0]] = stringToDate(entry[1])
+            daysLeft[entry[0]] = date.fromisoformat(entry[1]) - date.today()
     
     # display item and expiry date
-    for item in expiryDates.keys():
-        print(item + ": " + expiryDates[item].strftime("%x"))
-    
-# convert string in format YYYY/MM/DD to a datetime object
-def stringToDate(s):
-    date = [int(x) for x in s.split("/")]
-    return datetime.datetime(date[0], date[1], date[2])
+    for item in daysLeft.keys():
+        print(item + ": " + str(daysLeft[item].days) + " days left")
     
 if __name__ == "__main__":
     main()
